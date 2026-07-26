b2f9e33a9899d22b022adb3e5436d56b61a135e6
D <igothippie@yahoo.com>
2026-07-26 09:23:25 -0400

    favorite button working

--- build_index.py
+++ build_index.py
@@ -202,6 +202,7 @@ def build_snapshot_links(archive, character_name, site_base_url):
             "tags": tags,
             "automatic": not is_milestone,
             "story": metadata.get("story") is True,
+            "favorite": metadata.get("favorite") is True,
             "url": make_share_url(site_base_url, character_name, key),
             "canonicalUrl": make_share_url(site_base_url, character_name, canonical_key),
         })
